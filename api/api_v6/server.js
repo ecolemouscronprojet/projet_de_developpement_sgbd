7ec2cb449d578cee86cbc4346f2ea831b56d9cfc
Samuel Lassoie <samuel.lassoie+1@gmail.com>
2021-03-24 18:07:57 +0100

    wip

---
+++ api/api_v6/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+const databaseConnexion  = require("./database/connexion");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+//import controllers
+const users = require('./controllers/users');
+
+(async () => {
+    const db = await databaseConnexion();
+
+    // call controllers
+    users(app, db);
+    
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    });
+    
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    });
+})();
